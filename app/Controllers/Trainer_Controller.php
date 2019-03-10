6f80c3255fe3fb331a67f988bd916404d4e8e87e
William Bradshaw <wbradshaw4@my.bcit.ca>
2019-03-10 12:57:58 -0700

    Using existing lab work as base.

---
+++ app/Controllers/Trainer_Controller.php
@@ -0,0 +1,11 @@
+<?php namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+class Trainer_Controller extends Controller
+{
+    public function index()
+    {
+        echo 'Hello World!';
+    }
+}
