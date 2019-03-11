419f5189ba6b6948eec2c233e345f44776b0519b
William Bradshaw <wbradshaw4@my.bcit.ca>
2019-03-10 17:08:22 -0700

    Starting construction of model files.

--- app/Models/Trainer.php
+++ app/Models/Trainer.php
@@ -1,7 +1,18 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: jeffreystephen
  * Date: 2019-03-08
  * Time: 14:12
  */
+ 
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+ class TrainerModel extends Model
+ {
+         protected $DBGroup = 'default';
+		 
+ }
